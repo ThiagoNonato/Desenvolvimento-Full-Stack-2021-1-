5a3a1e9b891c1c0dcd78bd540df71d15f4876b59
Thiago Nonato <thiago_noonato.75@hotmail.com>
2021-08-17 13:07:33 -0300

    Exercicio 3

---
+++ Exercicios 3/src/main/java/br/ufg/inf/ctrl/HotelCtrl.java
@@ -0,0 +1,64 @@
+package br.ufg.inf.ctrl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.ufg.inf.business.HotelBusiness;
+import br.ufg.inf.entities.Hotel;
+import br.ufg.inf.exceptions.HotelException;
+
+public class HotelCtrl {
+    HotelBusiness business = new HotelBusiness();
+
+    public List<Hotel> findAll() {
+        List<Hotel> retorno = new ArrayList<Hotel>();
+
+        try {
+            retorno = business.findAll();
+        } catch (HotelException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return retorno;
+    }
+
+    public Hotel findById(Integer id) {
+        Hotel retorno = new Hotel();
+
+        try {
+            retorno = business.findById(id);
+        } catch (HotelException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return retorno;
+    }
+
+    public Hotel insert(Hotel hotel) {
+        try {
+            hotel = business.insert(hotel);
+        } catch (HotelException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return hotel;
+    }
+
+    public Hotel update(Hotel hotel) {
+        try {
+            hotel = business.update(hotel);
+        } catch (HotelException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return hotel;
+    }
+
+    public void delete(Integer id) {
+        try {
+            business.delete(id);
+        } catch (HotelException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
---
+++ Exercicios 3/src/main/java/br/ufg/inf/exceptions/HotelException.java
@@ -0,0 +1,10 @@
+package br.ufg.inf.exceptions;
+
+public class HotelException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public HotelException(String msg) {
+        super(msg);
+    }
+}
