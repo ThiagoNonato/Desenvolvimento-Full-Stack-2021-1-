5c624de716f1284ce60b01887e7a2f5e0789cdcc
Thiago Nonato <thiago_noonato.75@hotmail.com>
2021-11-08 15:25:14 -0300

    Exercícios Semana 10

---
+++ Exercicios 10/src/main/java/br/ufg/inf/exceptions/HospedagemException.java
@@ -0,0 +1,9 @@
+package br.ufg.inf.exceptions;
+
+public class HospedagemException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public HospedagemException(String msg) {
+        super(msg);
+    }
+}
